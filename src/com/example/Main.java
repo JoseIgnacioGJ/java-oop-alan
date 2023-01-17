86238be09572e59419139bdac8536eded615fee2
JoseIgnacioGJ <joigarja@gmail.com>
2023-01-17 11:02:42 +0100

    Cambio el ArrrayList por un vector

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -17,24 +17,24 @@ public class Main {
         p4.setPrecio(4.00);
         p5.setPrecio(3.00);
 
-        ArrayList<Producto> productos = new ArrayList();
-        productos.add(p1);
-        productos.add(p2);
-        productos.add(p3);
-        productos.add(p4);
-        productos.add(p5);
+        Producto productos[] = new Producto[5];
+        productos[0] = p1;
+        productos[1] = p2;
+        productos[2] = p3;
+        productos[3] = p4;
+        productos[4] = p5;
 
-        Double total = 0.0;
-        Double precioMaximo = 0.00;
-        for(int i = 0; i < productos.size(); i++)
+        double total = 0.0;
+        double precioMaximo = 0.00;
+        for(int i = 0; i < productos.length; i++)
         {
-            total += productos.get(i).getPrecio();
-            if(precioMaximo < productos.get(i).getPrecio())
-                precioMaximo = productos.get(i).getPrecio();
+            total += productos[i].getPrecio();
+            if(precioMaximo < productos[i].getPrecio())
+                precioMaximo = productos[i].getPrecio();
         }
 
         System.out.println("Total: " + total);
-        System.out.println("Media: " + total / productos.size());
+        System.out.println("Media: " + total / productos.length);
         System.out.println("Máximo: " + precioMaximo);
     }
 }
